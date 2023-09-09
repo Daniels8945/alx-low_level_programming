7e6e54e17edf4310d6c9cc14bb952c3b18e8284e
daniels8945 <obaj.daniels@gmail.com>
2023-09-08 18:38:56 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,33 +1,40 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main - prints all possible different combinations of three digits
- * Return: Always 0 (Success)
+ * main - Entry Point
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
+ * Return: 0
  */
+
 int main(void)
 {
-	int n, m, l;
+	int firstDigit = 0, seconDigit;
 
-	for (n = 48; n < 58; n++)
+	while (firstDigit <= 99)
 	{
-		for (m = 49; m < 58; m++)
+		seconDigit = firstDigit;
+		while (seconDigit <= 99)
 		{
-			for (l = 50; l < 58; l++)
+			if (seconDigit != firstDigit)
 			{
-				if (l > m && m > n)
+				putchar((firstDigit / 10) + 48);
+				putchar((firstDigit % 10) + 48);
+				putchar(' ');
+				putchar((seconDigit / 10) + 48);
+				putchar((seconDigit % 10) + 48);
+
+				if (firstDigit != 98 || seconDigit != 99)
 				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
+			++seconDigit;
 		}
+		++firstDigit;
 	}
 	putchar('\n');
+
 	return (0);
 }
