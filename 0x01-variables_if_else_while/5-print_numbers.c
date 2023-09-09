3c846264cb7101e5bd8bed349693d2a0d09a0a07
daniels8945 <obaj.daniels@gmail.com>
2023-09-08 17:29:12 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,6 +5,7 @@
  * Description: print all single digit numbers
  * Return: Always 0 (Success)
 */
+
 int main(void)
 {
 	int digit = 0;
@@ -15,5 +16,6 @@ int main(void)
 		++digit;
 	}
 	printf("\n");
+
 	return (0);
 }	
