a99a60d665dd6d95cbf8e64a814b7ee52b0b6845
daniels8945 <obaj.daniels@gmail.com>
2023-09-08 18:05:07 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,14 +10,12 @@
 
 int main(void)
 {
-	int a = 0;
+int a = 0;
+for (a = 0; a < 10; a++)
+{
+printf("%d", a);
+}
+printf("\n");
 
-	for (a = 0; a < 10; a++)
-	{
-		printf("%d", a);
-	}
-
-	printf("\n");
-
-	return (0);
+return (0);
 }	
