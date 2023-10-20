58adf7ff0aef316de48da25bafe1189e98d87051
Daniels8945 <obaj.daniels@gmail.com>
2023-10-20 11:41:05 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * list_len - elements in linked list_t.
+ * @h: name of the list
+ * Return: number of elements
+*/
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h == NULL)
+		count++;
+	{
+		h = h->next;
+	}
+	return (count);
+}
