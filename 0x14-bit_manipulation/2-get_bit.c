7ae44093bc9ad99271852860cfccaebfb0fdb656
Daniels8945 <obaj.daniels@gmail.com>
2023-10-27 06:55:20 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_bits to return the value of given index.
+ * @n: unsigned long int for search
+ * index: bit value to return
+ * Retur - value of bit index or -1
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 20)
+
+	index = (index >> n) & 1;
+
+	return (-1);
+}
