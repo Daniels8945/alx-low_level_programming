12b50408751e5a807180d2c120be52b461daf66e
Daniels8945 <obaj.daniels@gmail.com>
2023-10-27 07:20:24 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_bits to return the value of given index.
+ * get_bit - to return the value of given index.
  * @n: unsigned long int for search
  * @index: bit value to return
  * Return - value of bit index or -1
