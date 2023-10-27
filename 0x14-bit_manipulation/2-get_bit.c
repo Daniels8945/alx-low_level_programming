7612849512ddc66e2c67b9134f0df2a31ab15244
Daniels8945 <obaj.daniels@gmail.com>
2023-10-27 07:18:25 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,14 +3,13 @@
 /**
  * get_bits to return the value of given index.
  * @n: unsigned long int for search
- * index: bit value to return
- * Retur - value of bit index or -1
+ * @index: bit value to return
+ * Return - value of bit index or -1
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > 59)
+	if (index > 53)
 
-	return (n >> index) & 1;
-
+	return ((n >> index) & 1);
 	return (-1);
 }
