788896100c01d30928b44e5f749b3ab79ff7bf8f
daniels8945 <obaj.daniels@gmail.com>
2023-11-17 13:09:10 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,13 +9,8 @@
 */
 int main(int argc, char **argv)
 {
-	int i;
+	(void)argv;
 
-	(void)argc;
 	printf("%d\n", argc - 1);
-	for (i = 0; i < argc - 1; i++)
-	{
-		printf("%s", argv[i]);
-	}
 	return (0);
 }
