6c9c5c472ec81efd47cb7d810982eab055dd302c
daniels8945 <obaj.daniels@gmail.com>
2023-11-22 07:29:29 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,9 +15,9 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	str = (char *)malloc(sizeof(char) * size);
+	if (str == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		str[i] = c;
 	return (str);
-	if (str == NULL)
-		return (NULL);
 }
