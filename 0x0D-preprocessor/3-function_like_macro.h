76cc11ea86258df8cf7960c6876e6c879f5d651f
daniels8945 <obaj.daniels@gmail.com>
2023-11-27 13:43:35 -0800

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef 3_FUNCTION_LIKE_MACRO_H_
-#define 3_FUNCTION_LIKE_MACRO_H_
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _ 3_FUNCTION_LIKE_MACRO_H_
 
 #define ABS(x) ((x) < 0 ? (-x) : (x))
 #endif
